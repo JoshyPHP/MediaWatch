afacc664ae0f5b60971dd2c5068dec66b6139da0
s9e <s9e.dev@gmail.com>
2014-07-09 18:44:29 +0200

    Added tests

--- Test.php
+++ Test.php
@@ -101,8 +101,17 @@ class Test extends PHPUnit_Extensions_Selenium2TestCase
 
 			if (!empty($_SERVER['MAINTENANCE']))
 			{
+				$width  = (preg_match('/width="(\d+)"/', $html, $m))  ? $m[1] : 800;
+				$height = (preg_match('/height="(\d+)"/', $html, $m)) ? $m[1] : 600;
+
+				// Don't trust the height for dynamically resized iframes
+				if (strpos($html, 'style.height'))
+				{
+					$height = 600;
+				}
+
 				sleep(10);
-				file_put_contents($filepathExpected, $this->currentScreenshot());
+				$this->saveScreenshot($filepathExpected, $width, $height);
 
 				continue;
 			}
@@ -115,10 +124,7 @@ class Test extends PHPUnit_Extensions_Selenium2TestCase
 			{
 				sleep($sleep++);
 
-				$gd = imagecreatefromstring($this->currentScreenshot());
-				$gd = imagecrop($gd, ['x' => 0, 'y' => 0, 'width' => $width, 'height' => $height]);
-
-				imagepng($gd, $filepathActual, 5, PNG_NO_FILTER);
+				$this->saveScreenshot($filepathActual, $width, $height);
 
 				$output = exec(self::$dssim . ' ' . escapeshellarg($filepathExpected) . ' ' . escapeshellarg($filepathActual) . ' 2>&1', $arr, $error);
 
@@ -152,6 +158,13 @@ class Test extends PHPUnit_Extensions_Selenium2TestCase
 		}
 
 		$this->assertEmpty($errors);
+	}
+
+	protected function saveScreenshot($filepath, $width, $height)
+	{
+		$gd = imagecreatefromstring($this->currentScreenshot());
+		$gd = imagecrop($gd, ['x' => 0, 'y' => 0, 'width' => $width, 'height' => $height]);
+		imagepng($gd, $filepath, 5, PNG_NO_FILTER);
 	}
 
 	public function getBrowserRenderingTestsChunked()
@@ -440,6 +453,106 @@ class Test extends PHPUnit_Extensions_Selenium2TestCase
 			[
 				'mixcloud',
 				'http://www.mixcloud.com/OneTakeTapes/timsch-one-take-tapes-2/'
+			],
+			[
+				'nytimes',
+				'http://www.nytimes.com/video/technology/personaltech/100000002907606/soylent-taste-test.html'
+			],
+			[
+				'podbean',
+				'http://dialhforheroclix.podbean.com/e/dial-h-for-heroclix-episode-46-all-ya-need-is-love/'
+			],
+			[
+				'prezi',
+				'http://prezi.com/5ye8po_hmikp/10-most-common-rookie-presentation-mistakes/'
+			],
+			[
+				'rdio',
+				'http://rd.io/x/QcD7oTdeWevg/'
+			],
+			[
+				'rutube',
+				'http://rutube.ru/tracks/4118278.html?v=8b490a46447720d4ad74616f5de2affd'
+			],
+			[
+				'slideshare',
+				'http://www.slideshare.net/Slideshare/how-23431564'
+			],
+			[
+				'soundcloud-track',
+				'http://api.soundcloud.com/tracks/98282116'
+			],
+			[
+				'soundcloud-playlist',
+				'https://soundcloud.com/tenaciousd/sets/rize-of-the-fenix/'
+			],
+			[
+				'soundcloud-secret',
+				'https://soundcloud.com/matt0753/iroh-ii-deep-voice/s-UpqTm'
+			],
+			[
+				'spotify-track-uri',
+				'spotify:track:5JunxkcjfCYcY7xJ29tLai'
+			],
+			[
+				'spotify-track-url',
+				'http://open.spotify.com/track/4woJuZHL7xBDirHwWxkRrX'
+			],
+			[
+				'spotify-album',
+				'https://play.spotify.com/album/5OSzFvFAYuRh93WDNCTLEz'
+			],
+			[
+				'spotify-playlist',
+				'http://open.spotify.com/user/ozmoetr/playlist/4yRrCWNhWOqWZx5lmFqZvt'
+			],
+			[
+				'strawpoll',
+				'http://strawpoll.me/738091'
+			],
+			[
+				'teamcoco',
+				'http://teamcoco.com/video/73784/historian-a-scott-berg-serious-jibber-jabber-with-conan-obrien'
+			],
+			[
+				'ted',
+				'http://www.ted.com/talks/eli_pariser_beware_online_filter_bubbles.html'
+			],
+			[
+				'theatlantic',
+				'http://www.theatlantic.com/video/index/358928/computer-vision-syndrome-and-you/'
+			],
+			[
+				'traileraddict',
+				'http://www.traileraddict.com/the-amazing-spider-man-2/super-bowl-tv-spot'
+			],
+			[
+				'twitch',
+				'http://www.twitch.tv/amazhs/c/4493103'
+			],
+			[
+				'twitter',
+				'https://twitter.com/BarackObama/statuses/266031293945503744'
+			],
+			[
+				'ustream',
+				'http://www.ustream.tv/channel/ps4-ustream-gameplay'
+			],
+			[
+				'ustream-recorded',
+				'http://www.ustream.tv/recorded/40688256'
+			],
+			[
+				'vevo',
+				'http://www.vevo.com/watch/USUV71400682'
+			],
+			[
+				'vimeo',
+				'http://vimeo.com/67207222'
+			],
+			[
+				'vine',
+				'https://vine.co/v/bYwPIluIipH'
 			],
 		];
 	}
